f632f7fb526a77270b8f2493e66da83b282f1248
Constanza Pino <cpino012@gmail.com>
2023-08-21 21:16:48 -0400

    Added Try/catch

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { obtenerJoyas, filtrarJoyas } = require('./consultas')
+const { handleErrors } = require('./errors')
 const express = require('express')
 const app = express()
 
@@ -7,11 +8,25 @@ app.listen (3000, console.log("Servidor encendido en puerto 3000"))
 const reportarConsulta = async(req, res, next) =>{
     const parametros = req.query
     const url = req.url
-    console.log(`
-    Fecha: ${new Date()}
-    Ruta de la consulta: ${url}
-    Parámetros utilizados:
-    `, parametros)
+    if(Object.keys(parametros).length === 0){
+
+        console.log(`
+        Fecha: ${new Date()}
+        Ruta de la consulta: ${url}
+        Parámetros utilizados: No se usaron parametros
+        `)
+
+    }else{
+        
+        console.log(`
+        Fecha: ${new Date()}
+        Ruta de la consulta: ${url}
+        Parámetros utilizados:
+        `)
+        Object.keys(parametros).forEach(key => {
+            console.log(`${key}: ${parametros[key]}`)})
+        }
+
     next()
 }
 
@@ -32,15 +47,26 @@ const prepararHATEOAS = (inventario) =>{
 
 
 app.get("/joyas", reportarConsulta, async(req, res) =>{
-    const queryString = req.query
-    const inventario = await obtenerJoyas(queryString)
-    const HATEOAS = prepararHATEOAS(inventario)
-
-    return res.json(HATEOAS)
+    try{
+        const queryString = req.query
+        const inventario = await obtenerJoyas(queryString)
+        const HATEOAS = prepararHATEOAS(inventario)
+        return res.json(HATEOAS)
+    } catch (error) {
+        console.log(error)
+        const { status, message } = handleErrors(error.code)
+        return res.status(status).json({ok: false, result: message})
+    }
 });
 
 app.get('/joyas/filtros', reportarConsulta, async (req, res) => {
-    const queryStrings = req.query
-    const inventario = await filtrarJoyas(queryStrings)
-    res.json(inventario)
+    try{
+        const queryStrings = req.query
+        const inventario = await filtrarJoyas(queryStrings)
+        res.json(inventario)
+    } catch (error) {
+        console.log(error)
+        const { status, message } = handleErrors(error.code)
+        return res.status(status).json({ok: false, result: message})
+    }
 })
